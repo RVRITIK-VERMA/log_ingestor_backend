cd07068092a9a0f289445a560c86e34a55dfb92e
RVRITIK-VERMA <ritikverma431@gmail.com>
2023-11-19 21:30:42 +0530

    normal url added

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var corsOption = {
 }
 app.use(cors(corsOption));
 require('./src/routes')(app);
+app.get('/',(req,res)=>{
+    return res.status(200).send({ status: true, data: 'homescreen' });
+})
 app.listen(port,()=>{
     console.log(`server is listening to port ${port}`);
 });
