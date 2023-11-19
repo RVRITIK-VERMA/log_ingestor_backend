9517c9b725fdf1eddc3da3798070e1f30b5ed826
RVRITIK-VERMA <ritikverma431@gmail.com>
2023-11-19 21:35:09 +0530

    cors resolving

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ const cors = require('cors');
 
 app.use(express.json());
 var corsOption = {
-    // origin : ['*',"https://log-ingestor-backend.vercel.app/","http://localhost:3000/"]
-    origin : '*',
-    preflightContinue : true,
+    origin : '*'
 }
 app.use(cors(corsOption));
 require('./src/routes')(app);
-app.get('/',(req,res)=>{
-    return res.status(200).send({ status: true, data: 'homescreen' });
-})
+
 app.listen(port,()=>{
     console.log(`server is listening to port ${port}`);
 });
